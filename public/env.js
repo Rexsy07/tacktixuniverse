76539d39625e89061a4bccd4fa76669f7d51a17f
Rexsy07 <samuelezemr@gmail.com>
2025-09-24 02:32:47 +1000

    Inject runtime Supabase env via public/env.js and include it in index.html to fix missing anon key on GitHub Pages

---
+++ public/env.js
@@ -0,0 +1,5 @@
+window.__ENV = {
+  VITE_SUPABASE_PROJECT_ID: "cceexamgzprswhvaozem",
+  VITE_SUPABASE_URL: "https://cceexamgzprswhvaozem.supabase.co",
+  VITE_SUPABASE_PUBLISHABLE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjZWV4YW1nenByc3dodmFvemVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzMjQzMzUsImV4cCI6MjA3MzkwMDMzNX0.Sf3HS6iUtj13pvv4K1D31Xxa_ighBH7VnDd8P2LODCI"
+};
